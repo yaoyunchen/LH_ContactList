8da6795f253bebe9642b460c0dbe500e2611f819
Andy Yao <a_n_d_y_y_a_o@hotmail.com>
2016-01-12 04:55:09 +0000

    Updated to include ARGV when running program.

--- contact_list.rb
+++ contact_list.rb
@@ -6,17 +6,56 @@ class ContactList
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
 
   def initialize 
-    main_menu
+    #main_menu
+    argv_menu
   end
+
+  def argv_menu
+    case ARGV[0]
+      when "new"
+        if ARGV[1] == nil
+          name = get_input("Enter the contact's full name.")
+          email = get_input("Enter contact's email address.")
+        else
+          name = ARGV[1]
+          if ARGV[2] == nil
+            email = get_input("Enter contact's email address.")
+          else
+            email = ARGV[2]
+          end
+        end
+        puts Contact.create(name, email)
+      when "list"
+        puts Contact.all
+      when "show"
+        if ARGV[1] == nil
+          puts Contact.find(get_input("Please enter the ID."))
+        else
+          puts Contact.find(ARGV[1])
+        end
+      when "search"
+        if ARGV[1] == nil
+          puts Contact.search(get_input("Please enter search term."))
+        else
+          puts Contact.search(ARGV[1])
+        end
+      else
+        main_menu
+    end
+  end
+
+  def get_input(string)
+    puts string
+    $stdin.gets.chomp
+  end
+
 
   def main_menu
     puts "Here is a list of available commands:
       new     - Create a new contact
       list    - List all contacts
       show    - Show a contact
-      search  - Search contacts 
-      "
-      # input, name, email, password = ARGV
+      search  - Search contacts"
     case gets.chomp
       when "new"
         puts "Enter the contact's full name."
@@ -36,16 +75,6 @@ class ContactList
       else
     end
   end
-
-  # def list_contacts
-  #   puts "List contacts."
-  # end
-
-
-
-
-
-
 
   #Creates instance of the class to run program.
   ContactList.new
