76b5833046786ef6d55464a7ddad7e6ec4787ebc
Andy Yao <a_n_d_y_y_a_o@hotmail.com>
2016-01-12 00:31:16 +0000

    Added menu, user input.

--- contact_list.rb
+++ contact_list.rb
@@ -5,4 +5,29 @@ class ContactList
 
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
 
+  def initialize 
+    main_menu
+  end
+
+  def main_menu
+    puts "Here is a list of available commands:
+      new     - Create a new contact
+      list    - List all contacts
+      show    - Show a contact
+      search  - Search contacts 
+      "
+
+    case gets.chomp
+      when "new"
+        puts "New contact."
+      when "list"
+        puts "List contacts."
+      when "show"
+        puts "Show contact."
+      when "search"
+        puts "Search contact."
+      else
+    end
+  end
+  ContactList.new
 end
