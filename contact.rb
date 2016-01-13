2c67c736e0feb5da18aabdc49cd9107d19a08025
Andy Yao <a_n_d_y_y_a_o@hotmail.com>
2016-01-13 03:13:21 +0000

    Refactored code.

--- contact.rb
+++ contact.rb
@@ -5,6 +5,7 @@ class Contact
 
   attr_accessor :name, :email, :phone
   attr_reader :ID
+  @@file = 'contacts.csv'
 
   def initialize(name, email)
     # TODO: Assign parameter values to instance variables.
@@ -16,80 +17,59 @@ class Contact
 
   # Provides functionality for managing a list of Contacts in a database.
   class << self
-    # Returns an Array of Contacts loaded from the database.
-    def all
-      ret_str = ""
-      count = 0
-
-      csv_to_hash.each do |row|
-        ret_str << build_string(row)
-        count += 1
-      end
-      
-      ret_str << "---\n#{count} records total"
-      ret_str
-    end
-
-
     # Creates a new contact, adding it to the database, returning the new contact.
     def create(name, email, phone = nil)
-      # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
       count = 1
-      CSV.foreach('contacts.csv') do |row|
+      CSV.foreach(@@file) do |row|
         count += 1
       end
-      CSV.open('contacts.csv', 'a') do |csv|
+      CSV.open(@@file, 'a') do |csv|
         phone.nil? ? csv << [count, name, email] : csv << [count, name, email, phone]
       end
       "New contact #{name} (#{email}) added at ID: #{count}."
     end
 
 
-    # Returns the contact with the specified id. If no contact has the id, returns nil.
-    def find(id)
-      # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
-      ret_str = ""
-
-      csv_to_hash.each do |row|
-        if row[:ID] == id
-          ret_str << build_string(row)
-        end
-      end
-
-      ret_str = "ID not found." if ret_str == ""
-      ret_str
-    end
-
-    # Returns an array of contacts who match the given term.
-    def search(term)
-      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
+    #Goes through contacts and carry out actions based on inputs.
+    def loop_through_contacts(action, param = nil)
       ret_str = ""
       count = 0
 
       csv_to_hash.each do |row|
-        unless row.values.grep(/#{term}/i) == []
-          ret_str << build_string(row)
-          count += 1
+        case action
+          when "all"
+            ret_str << build_string(row)
+            count += 1
+          when "show"
+            ret_str << build_string(row) if row[:ID].to_i == param.to_i
+          when "search"  
+            unless row.values.grep(/#{param}/i) == []
+              ret_str << build_string(row)
+              count += 1
+            end
         end
       end
-      
-      ret_str << "---\n#{count} records total"
+
+      ret_str << "---\n#{count} records total" unless action == "show"
       ret_str
     end
 
+
     #Checks if the entered email exists in the contact list already.
     def email_exists?(email)
-      CSV.foreach('contacts.csv') do |row|
+      CSV.foreach(@@file) do |row|
         return true if row.any? {|element| element =~ /#{email}/i}
       end
     end
 
-    #Converts the CSV file into a hash for easier usage.
+
+    #Converts the CSV file into a hash.
     def csv_to_hash
       keys = [:ID, :name, :email, :phone]
       # CSV.parse(input).map {|ele| Hash[keys.zip(ele)]}
-      CSV.read('contacts.csv').map{|a| Hash[keys.zip(a)]}
+      CSV.read(@@file).map{|a| Hash[keys.zip(a)]}
     end
+
 
     #Builds the return string to be displayed to the user.
     def build_string(row)
--- contact_list.rb
+++ contact_list.rb
@@ -12,93 +12,115 @@ class ContactList
   def argv_menu
     case ARGV[0]
       when "new"
-        if ARGV[1] == nil
-          name = get_input("full name")
-          email = get_input("email address")
-        else
-          name = ARGV[1]
-          if ARGV[2] == nil
-            email = get_input("email address")
-          else
-            email = ARGV[2]
-
-            phone = []
-            phone_numbers = ARGV.drop(3)
-
-            until phone_numbers.empty?
-              phone << phone_numbers[0]
-              phone_numbers = phone_numbers.drop(1)
-            end
-          end
-        end
-        begin
-          raise EmailExistsError if Contact.email_exists?(email)
-          phone = nil if phone == ""
-          puts Contact.create(name, email, phone.join(",").gsub(",","/"))
-        rescue EmailExistsError
-          puts "The entered email exists in the contact list already."
-        end
+        puts create_new_contact
       when "list"
-        puts Contact.all
+        #puts Contact.all
+        puts Contact.loop_through_contacts("all")
       when "show"
-        if ARGV[1] == nil
-          puts Contact.find(get_input("ID"))
-        else
-          puts Contact.find(ARGV[1])
-        end
+        puts do_show
       when "search"
-        if ARGV[1] == nil
-          puts Contact.search(get_input("search term"))
-        else
-          puts Contact.search(ARGV[1])
-        end
+        puts do_search
       else
         main_menu
     end
   end
 
-
-  def get_input(string)
-    puts "Please enter #{string}."
-    $stdin.gets.chomp
-  end
-
-
+  #Loads a menu if no ARGV provided when running.
   def main_menu
     puts "Here is a list of available commands:
       new     - Create a new contact
       list    - List all contacts
       show    - Show a contact
-      search  - Search contacts"
-  
+      search  - Search contacts"  
     case gets.chomp
       when "new"
-        puts "Enter the contact's full name."
-        name = gets.chomp
-        
-        puts "Enter contact's email address."
-        email = gets.chomp
-
-        begin  
-          raise EmailExistsError if Contact.email_exists?(email)
-          puts Contact.create(name, email)
-        rescue EmailExistsError
-          puts "The entered email exists in the contact list already."
-        end
+        puts create_new_contact
       when "list"
-        puts Contact.all
+        #puts Contact.all
+        puts Contact.loop_through_contacts("all")
       when "show"
-        puts "Please enter ID."
-        puts Contact.find(gets.chomp)
+        #puts Contact.find(get_input("ID"))
+        puts do_show
       when "search"
-        puts "Please enter search term."
-        puts Contact.search(gets.chomp)
+        #puts Contact.search(get_input("search term"))
+        puts do_search
     end
   end
 
 
+  #Show contact, given the ID.
+  def do_show
+    ARGV[1] == nil ? Contact.loop_through_contacts("show", get_input("ID")) : Contact.loop_through_contacts("show", ARGV[1])
+  end
+
+
+  #Search contacts for a given term.
+  def do_search
+    ARGV[1] == nil ? Contact.loop_through_contacts("search", get_input("search term")) : Contact.loop_through_contacts("search", ARGV[1])
+  end
+  
+
+  #Used to prompt user for input and return the string the user enters.
+  def get_input(string)
+    puts "Please enter #{string}."
+    $stdin.gets.chomp
+  end
+
+  #Gets the name for new contact.
+  def get_name
+    ARGV[1] == nil ? get_input("full name") : ARGV[1]
+  end
+
+  #Gets the email for new contact.
+  def get_email
+    ARGV[2] == nil ? get_input("email") : ARGV[2]
+  end
+
+  #Gets every input after email add add them to a phone number array.
+  def get_phone_numbers
+    phone_array = []
+
+    phone_numbers = ARGV.drop(3)
+    if phone_numbers[0] == nil
+      puts "Please enter phone numbers in 'TYPE_01:111-111-1111/TYPE_02:222-222-2222' format, or ENTER to continue."
+      phone_numbers = $stdin.gets.chomp
+    else
+    until phone_numbers.empty?
+      phone_array << phone_numbers[0]
+      phone_numbers = phone_numbers.drop(1)
+    end
+    phone_array.join("/")
+    end
+  end
+
+
+  #Used to create a new contact if inputs were given, or prompt user for inputs first if not.
+  def create_new_contact
+    begin
+      name = get_name
+      email = get_email
+      phone_numbers = get_phone_numbers
+
+      #Makes sure entered email is in correct email format.
+      raise InvalidEmailError unless email.include?("@")
+      #Check if entered email exists already in the CSV.
+      raise EmailExistsError if Contact.email_exists?(email)
+
+      Contact.create(name, email, phone_numbers)
+    rescue InvalidEmailError
+      "The entered email is in an invalid format."
+    rescue EmailExistsError
+      "The entered email exists in the contact list already."
+    end
+  end
+
+  
   class EmailExistsError < StandardError
   
+  end
+
+  class InvalidEmailError < StandardError
+
   end
 
   #Creates instance of the class to run program.
