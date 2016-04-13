abefeaa3c10a3b161694389122092acc91881e8f
Andy Yao <a_n_d_y_y_a_o@hotmail.com>
2016-04-13 13:21:15 -0700

    removed commented out code

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
-  has_many :numbers, :dependent => :destroy
+  has_many :numbers, dependent: :destroy
 
   validates :name, :email, presence: true
   validates :email, uniqueness: true
@@ -23,86 +23,5 @@ class Contact < ActiveRecord::Base
   end
 end
 
-
-
-
-
-# # Represents a person in an address book.
-# class Contact
-
-#   attr_accessor :name, :email, :phone
-#   attr_reader :ID
-#   @@file = 'contacts.csv'
-
-#   def initialize(name, email)
-#     # TODO: Assign parameter values to instance variables.
-#     @ID = ID
-#     @name = name
-#     @email = email
-#     @phone = phone
-#   end
-
-#   # Provides functionality for managing a list of Contacts in a database.
-#   class << self
-#     # Creates a new contact, adding it to the database, returning the new contact.
-#     def create(name, email, phone = nil)
-#       count = 1
-#       CSV.foreach(@@file) do |row|
-#         count += 1
-#       end
-#       CSV.open(@@file, 'a') do |csv|
-#         phone.nil? ? csv << [count, name, email] : csv << [count, name, email, phone]
-#       end
-#       "New contact #{name} (#{email}) added at ID: #{count}."
-#     end
-
-
-#     #Goes through contacts and carry out actions based on inputs.
-#     def loop_through_contacts(action, param = nil)
-#       ret_str = ""
-#       count = 0
-
-#       csv_to_hash.each do |row|
-#         case action
-#           when "all"
-#             ret_str << build_string(row)
-#             count += 1
-#           when "show"
-#             ret_str << build_string(row) if row[:ID].to_i == param.to_i
-#           when "search"  
-#             unless row.values.grep(/#{param}/i) == []
-#               ret_str << build_string(row)
-#               count += 1
-#             end
-#         end
-#       end
-
-#       ret_str << "---\n#{count} records total" unless action == "show"
-#       ret_str
-#     end
-
-
-#     #Checks if the entered email exists in the contact list already.
-#     def email_exists?(email)
-#       CSV.foreach(@@file) do |row|
-#         return true if row.any? {|element| element =~ /#{email}/i}
-#       end
-#     end
-
-
-#     #Converts the CSV file into a hash.
-#     def csv_to_hash
-#       keys = [:ID, :name, :email, :phone]
-#       # CSV.parse(input).map {|ele| Hash[keys.zip(ele)]}
-#       CSV.read(@@file).map{|a| Hash[keys.zip(a)]}
-#     end
-
-
-#     #Builds the return string to be displayed to the user.
-#     def build_string(row)
-#       "#{row[:ID]}: #{row[:name]} (#{row[:email]}) #{row[:phone]}\n"
-#     end
-#   end
-# end
 
 
--- lib/contact_list.rb
+++ lib/contact_list.rb
@@ -48,7 +48,7 @@ class ContactList
     end
   end
 
-
+  # Adding a new contact to the list.
   def new_contact
     ARGV[1] == nil ? name = get_input("name") : name = ARGV[1] 
     ARGV[2] == nil ? email = get_input("email") : email = ARGV[2]
@@ -61,7 +61,8 @@ class ContactList
       p @contact.errors.to_a 
     end
   end
-  
+
+  # Save phone numbers if they exist
   def save_numbers(phone_numbers)
     unless phone_numbers == nil
       phone_numbers.each do |number|
@@ -72,11 +73,12 @@ class ContactList
     end
   end
 
+  # Returns the list of contacts
   def list_contacts
     get_results(Contact.includes(:numbers))
   end
 
-
+  # Show contact information for given ID.
   def show_contacts
     id = ARGV[1] ? ARGV[1] : get_input("ID") 
 
@@ -84,7 +86,7 @@ class ContactList
     .where("contacts.id = ?", [id]))
   end
 
-
+  # Search for a contact with a given search term.
   def search_contacts
     term = ARGV[1] ? ARGV[1] : get_input("search term")
 
@@ -94,7 +96,7 @@ class ContactList
     get_results(contacts)
   end
 
-
+  # Deletes a contact from the list.
   def destroy_contact
     id = ARGV[1] ? ARGV[1] : get_input("ID")
     contact = Contact.find(id)
@@ -103,19 +105,19 @@ class ContactList
   end
 
 
-  #Checks if the user input is an integer and set as -1 if not.
+  # Checks if the user input is an integer and set as -1 if not.
   def is_integer(input)
     input if Integer(input) rescue -1
   end
 
 
-  #Used to prompt user for input and return the string the user enters.
+  # Used to prompt user for input and return the string the user enters.
   def get_input(string)
     puts "Please enter #{string}."
     $stdin.gets.chomp
   end
 
-
+  # Check if there are phone numbers to be saved.
   def get_phone_numbers
     phone_numbers = ARGV.drop(3)
 
@@ -124,7 +126,7 @@ class ContactList
     phone_array = phone_numbers[0] ? argv_numbers(phone_numbers) : entered_numbers
   end
 
-
+  # If there are no phone numbers entered in terminal, ask the user if there is any.
   def entered_numbers
     puts "Please enter phone numbers in 'TYPE_01:111-111-1111/TYPE_02:222-222-2222' format, or ENTER to continue."
     phone_numbers = $stdin.gets.chomp
@@ -138,7 +140,7 @@ class ContactList
     end
   end
 
-
+  # If phone numbers are entered into the terminal, clean up the numbers.
   def argv_numbers(phone_numbers)
     phone_array = []
     temp_array = []
@@ -153,7 +155,7 @@ class ContactList
     phone_array
   end
 
-
+  # Check if there the contact exists before return.
   def get_results(contacts)
     puts contacts.empty? ? "No results found." : contacts.to_a
   end
